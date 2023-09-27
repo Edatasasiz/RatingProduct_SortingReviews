b42b19bdcae9f6ce90366c037cadf4ad0ffce1b0
Eda Tasasız <101572852+Edatasasiz@users.noreply.github.com>
2023-09-27 12:25:41 +0300

    Update Rating_Product&Sorting_Reviews_in_Amazon.py

--- Rating_Product&Sorting_Reviews_in_Amazon.py
+++ Rating_Product&Sorting_Reviews_in_Amazon.py
@@ -12,39 +12,6 @@ pd.set_option('display.width', 500)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-###################################################
-# İş Problemi
-###################################################
-
-# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
-# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
-# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
-# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
-# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
-# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
-
-###################################################
-# Veri Seti Hikayesi
-###################################################
-
-# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
-# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
-
-# Değişkenler:
-# reviewerID: Kullanıcı ID’si
-# asin: Ürün ID’si
-# reviewerName: Kullanıcı Adı
-# helpful: Faydalı değerlendirme derecesi
-# reviewText: Değerlendirme
-# overall: Ürün rating’i
-# summary: Değerlendirme özeti
-# unixReviewTime: Değerlendirme zamanı
-# reviewTime: Değerlendirme zamanı Raw
-# day_diff: Değerlendirmeden itibaren geçen gün sayısı
-# helpful_yes: Değerlendirmenin faydalı bulunma sayısı
-# total_vote: Değerlendirmeye verilen oy sayısı
-
-
 
 ###################################################
 # GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
@@ -195,7 +162,7 @@ df["wilson_lower_bound"] = df.apply(lambda x: wilson_lower_bound(x["helpful_yes"
 df.sort_values("wilson_lower_bound", ascending=False).head(20)
 
 # WLB, hem yararlı oyları hem frekansı göz önünde bulundurur.
-# Sonuçlara baktığımızda olumlu oy sayısı ve değerlenmesi yüksek olanları gözlemledik.
+# Sonuçlara baktığımızda olumlu oy sayısı ve değerlendirmesi yüksek olanları gözlemledik.
 # Olumsuz oy almayan az sayıdaki yorumları da görebildik.
 
 
